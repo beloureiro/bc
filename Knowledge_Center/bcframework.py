cec5a24e0aa2ca34fb02ac2170395178db9c241d
Bernhard <beloureiro@gmail.com>
2024-09-13 00:32:39 +0100

    Componentizado o category feedback do Dasb1

--- Knowledge_Center/bcframework.py
+++ Knowledge_Center/bcframework.py
@@ -4,6 +4,7 @@ from fase2 import develop_nlp_algorithm
 from fase3 import prioritize_breakdown_causes
 from fase4 import develop_actions
 
+
 def run_bcframework():
     st.title("Business Case Web App")
     st.write("Welcome to the Business Case Web App!")
@@ -20,7 +21,7 @@ def run_bcframework():
     """)
     st.image("logo/bc_framework.png")  # Caminho da imagem
     st.write("___")  # Linha de separação
-    
+
     st.write("⚠️ Technical Explanation of the Framework Approach:")
     st.write("""
     The process design was prioritized, establishing the premise that this initial step is fundamental for categorizing and analyzing the data. This approach highlights the importance of understanding the operation before performing categorization, ensuring that the relationship between the data and the process is clear.
@@ -31,54 +32,57 @@ def run_bcframework():
 
     This approach provides an integrated view, where data analysis is connected to process management, contributing to more effective and targeted categorization.
     """)
-    
+
     st.write("___")  # Linha de separação
     st.header("Web App Overview")
-    
-    st.write("Use the navigation menu on the left to explore each stage of the web app in detail.")
+
+    st.write(
+        "Use the navigation menu on the left (⬅️) to explore each stage of the web app in detail.")
 
     col1, col2 = st.columns(2)
-    
+
     with col1:
         st.subheader("1. Model the Patient Lifecycle and Touchpoints")
         st.write("""
         Mapping the patient lifecycle and identifying critical touchpoints within the process.
         Output: **Touchpoint Mapping & Diagram** – A comprehensive diagram highlighting key touchpoints to optimize patient care.
         """)
-        
-        st.subheader("2. Develop and Test a Natural Language Processing Algorithm")
+
+        st.subheader(
+            "2. Develop and Test a Natural Language Processing Algorithm")
         st.write("""
         A Python-based NLP algorithm is developed and conceptually tested to categorize and analyze patient feedback.
         Output: **Sentiment Analysis** – Detailed sentiment analysis, providing insights into patient emotions.
         """)
-    
+
     with col2:
         st.subheader("3. Prioritize and Breakdown Causes")
         st.write("""
         Correlates the sentiment data with specific processes to identify key pain points in the patient journey.
         Output: **Sentiment-Process Correlation Status** – A status report on the correlation between sentiments and processes, identifying areas for improvement.
         """)
-        
+
         st.subheader("4. Develop Actions Based on Identified Causes")
         st.write("""
         Focuses on conducting a root cause analysis and creating a strategic dashboard.
         Output: **Root Cause Analysis & Dashboard** – Actionable insights and a visual dashboard to drive process improvements and support growth.
         """)
-    
+
     # # Chamada das funções para as quatro etapas
     # model_patient_lifecycle()
-    
+
     # st.write("___")  # Linha de separação
-    
+
     # develop_nlp_algorithm()
-    
+
     # st.write("___")  # Linha de separação
-    
+
     # prioritize_breakdown_causes()
-    
+
     # st.write("___")  # Linha de separação
-    
+
     # develop_actions()
+
 
 if __name__ == "__main__":
     run_bcframework()
