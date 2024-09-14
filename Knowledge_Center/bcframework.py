a0a41e6616376bf9c14cbdf0f212cf090aa612da
Bernhard <beloureiro@gmail.com>
2024-09-14 15:39:24 +0100

    Business Case Web App updatedp

--- Knowledge_Center/bcframework.py
+++ Knowledge_Center/bcframework.py
@@ -8,27 +8,24 @@ from fase4 import develop_actions
 def run_bcframework():
     st.title("Business Case Web App")
     st.write("Welcome to the Business Case Web App!")
-    st.write("""
-    This web app was developed to explain the Business Case Framework, which is structured in four stages. 
+    st.markdown("""
+    This web app was developed to explain the <span style='color: #00c3a5;'><strong>Business Case Framework</strong></span>, which is structured in four stages. 
     The framework begins with mapping the patient lifecycle within the process and identifying critical touchpoints to understand the patient journey. 
     Next, a Python-based NLP algorithm is developed and conceptually tested to categorize and analyze user feedback. 
     Following this, the framework correlates sentiment data with specific processes to uncover key pain points. 
     The final objective is to conduct a root cause analysis, leading to the development of a strategic action plan aimed at improving patient care, 
     while subtly identifying internal improvement opportunities and potential revenue enhancements.
-    """)
+    """, unsafe_allow_html=True)
     st.write("""
-    Below, you will find more detailed information on each of the four stages of the web app. This will help you understand the processes involved and how each step contributes to the overall goal of improving patient care.
+    Below, you will find more detailed information on each of the four stages of the web app.
     """)
     st.image("logo/bc_framework.png")  # Caminho da imagem
     st.write("___")  # Linha de separação
 
-    st.write("⚠️ Technical Explanation of the Framework Approach:")
     st.write("""
     The process design was prioritized, establishing the premise that this initial step is fundamental for categorizing and analyzing the data. This approach highlights the importance of understanding the operation before performing categorization, ensuring that the relationship between the data and the process is clear.
 
     Modeling the patient lifecycle and identifying critical touchpoints allow data categorization to be aligned with operational reality. This perspective organizes the data in a structured way, directly connecting it to the process's opportunity gaps.
-
-    Although the Business Case guidance specifies that the presentation should follow the sequence "Data Categorization," "Pain Point Analysis," "Root Cause Hypothesis," and "Action Plan Development," the process design was prioritized to ensure that all subsequent analyses are conducted in a well-defined context. This way, the established categories are more robust and aligned with the operational flow.
 
     This approach provides an integrated view, where data analysis is connected to process management, contributing to more effective and targeted categorization.
     """)
@@ -36,8 +33,9 @@ def run_bcframework():
     st.write("___")  # Linha de separação
     st.header("Web App Overview")
 
-    st.write(
-        "Use the navigation menu on the left (⬅️) to explore each stage of the web app in detail.")
+    st.markdown("""
+    <span style='color: #00c3a5;'><strong>Use the navigation menu on the left (⬅️) to explore each stage of the web app in detail.</strong></span>
+    """, unsafe_allow_html=True)
 
     col1, col2 = st.columns(2)
 
